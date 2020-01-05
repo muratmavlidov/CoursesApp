87531b1c2ac06212c5823b7f5f60f23471b8e6d3
Murat Mavlidov <mmavlidov96@mail.ru>
2020-01-05 14:59:35 +0300

    init project, setting node server and added materialize css plugin

---
+++ index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port: ${PORT}`);
+});
