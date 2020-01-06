8fb0fc021cda23d409c5c97160a3859025cc118f
Murat Mavlidov <mmavlidov96@mail.ru>
2020-01-06 18:52:40 +0300

    create model for working with local db

--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const Course = require('../models/course.js');
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -6,6 +7,15 @@ router.get('/', (req, res) => {
     title: 'Добавит курс',
     isAdd: true
   })
+});
+
+router.post('/', async (req, res) => {
+  const { title, price, img } = req.body;
+  const course = new Course(title, price, img);
+
+  await course.save();
+  
+  res.redirect('/courses');
 });
 
 module.exports = router
