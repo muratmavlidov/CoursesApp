47292b94d9f5492fba793cc39fea7be77049f574
Murat Mavlidov <mmavlidov96@mail.ru>
2020-01-08 02:44:27 +0300

    add editing functional

--- routes/courses.js
+++ routes/courses.js
@@ -1,17 +1,34 @@
 const { Router } = require('express');
 const Course = require('../models/course.js');
+
 const router = Router();
 
 router.get('/', async (req, res) => {
-
   const courseList = await Course.getAll();
-
   res.render('courses', {
     title: 'Курсы',
     isCourses: true,
     courseList
   })
 });
+
+router.get('/:id/edit', async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect('/')
+  }
+  
+  const course = await Course.getCourse(req.params.id);
+
+  res.render('course-edit', {
+    title: `Редактировать ${course.title}`,
+    ...course
+  })
+});
+
+router.post('/edit', async (req, res) => {
+  await Course.update(req.body);
+  res.redirect('/courses');
+})
 
 router.get('/:id', async (req, res) => {
   const course = await Course.getCourse(req.params.id);
