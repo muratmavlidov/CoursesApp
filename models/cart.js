7e8ec05838fd45be33b6dbd980c87cb8a210579c
Murat Mavlidov <mmavlidov96@mail.ru>
2020-01-09 03:32:54 +0300

    async request processing

--- models/cart.js
+++ models/cart.js
@@ -31,6 +31,29 @@ class Cart {
     });
   }
 
+  static async remove(id) {
+    const cart = await Cart.fetch();
+    const idx = cart.courses.findIndex(c => c.id === id);
+    const course = cart.courses[idx];
+
+    if (course.count === 1) {
+      cart.courses = cart.courses.filter(c => c.id !== id);
+    } else {
+      cart.courses[idx].count--;
+    }
+
+    cart.price -= course.price;
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(dataPath, JSON.stringify(cart), (err) => {
+        if (err) reject(err);
+        else {
+          resolve(cart);
+        }
+      });
+    });
+  }
+
   static async fetch() {
     return new Promise((resolve, reject) => {
       fs.readFile(dataPath, 'utf-8', (err, content) => {
--- public/app.js
+++ public/app.js
@@ -5,4 +5,24 @@ priceElements.forEach((el) => {
     currency: 'usd',
     style: 'currency'
   }).format(el.textContent);
-})
+});
+
+
+// Delete cart item
+const cart = document.querySelector('#cart');
+
+if (cart) {
+  cart.addEventListener('click', ({target}) => {
+    if (target.matches('.js-remove')) {
+      const id = target.dataset.id
+      
+      fetch('/cart/remove/' + id, {
+        method: 'delete'
+      })
+      .then(res => res.json())
+      .then(cart => {
+        console.log(cart);
+      })
+    }
+  })
+}
