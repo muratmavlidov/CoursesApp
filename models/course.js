d448779cc1c6ba67a284110e8c869e952e1a6bb2
Murat Mavlidov <mmavlidov96@mail.ru>
2020-01-12 15:20:57 +0300

    getting id with objectId

--- models/course.js
+++ models/course.js
@@ -1,7 +1,6 @@
 const { Schema, model } = require('mongoose');
 
 const course = new Schema({
-  _id: String,
   title: {
     type: String,
     required: true
--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
 const Course = require('../models/course.js');
+const ObjectID = require('mongodb').ObjectID; 
 
 const router = Router();
 
+const getId = (id) => {
+  return new ObjectID(id);
+}
+
 router.get('/', async (req, res) => {
   const courseList = await Course.find();
+
   res.render('courses', {
     title: 'Курсы',
     isCourses: true,
@@ -17,22 +23,27 @@ router.get('/:id/edit', async (req, res) => {
     return res.redirect('/')
   }
   
-  const id = `'${req.params.id}'` // check it
+  const id = getId(req.params.id)
+
   const course = await Course.findById(id);
+  const { title, price, img } = course;
   res.render('course-edit', {
-    ...course
+    title,
+    price,
+    img
   })
 });
 
 router.post('/edit', async (req, res) => {
+  console.log(req);
   const { id } = req.body;
-  delete req.body.id;
   await Course.findOneAndUpdate(id, req.body);
   res.redirect('/courses');
 })
 
 router.get('/:id', async (req, res) => {
-  const course = await Course.findById(req.params.id);
+  const id = getId(req.params.id)
+  const course = await Course.findById(id);
   res.render('course', {
     layout: 'empty',
     ...course
